206ebe5a9a8e580ef3d0d653250e2d56ef4cc47c
Ilya Etingof <etingof@gmail.com>
2019-02-25 09:32:56 +0100

    Fix PDU consistency check in notification originator

--- pysnmp/entity/rfc3413/ntforg.py
+++ pysnmp/entity/rfc3413/ntforg.py
@@ -381,7 +381,7 @@ class NotificationOriginator:
         for idx, varBind in enumerate(varBinds[1:]):
             if varBind[0] == sysUpTime:
                 varBinds[0] = varBind
-                del varBinds[idx]
+                del varBinds[idx + 1]
                 break
 
         if len(varBinds) < 2:
@@ -393,7 +393,7 @@ class NotificationOriginator:
         # Search for and reposition snmpTrapOID if it's elsewhere
         for idx, varBind in enumerate(varBinds[2:]):
             if varBind[0] == snmpTrapOID:
-                del varBinds[idx]
+                del varBinds[idx + 2]
                 if varBinds[1][0] == snmpTrapOID:
                     varBinds[1] = varBind
                 else:
