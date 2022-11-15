93eabe7cba7495cdfda2b15df710fc4d82de1213
Lex Li <support@lextm.com>
2022-11-15 03:30:08 -0500

    Fixed GitHub repo and added GA4.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,7 +121,7 @@ html_theme_options = {
     'logo': 'logo.svg',
     'description': '<p align=left><i><b>Brewing free software for the greater good</i></b></p>',
     'show_powered_by': False,
-    'github_user': 'etingof',
+    'github_user': 'lextudio',
     'github_repo': 'pysnmp',
     'fixed_sidebar': True,
 }
