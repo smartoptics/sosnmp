52d3129817e5c0e91bd24d315230f7c253706301
Lex Li <support@lextm.com>
2023-02-11 15:36:34 -0500

    Fixed sitemap.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx_copybutton',
 ]
 
-html_baseurl = 'https://www.pysnmp.com/pysnmp'
+html_baseurl = 'https://www.pysnmp.com/pysnmp/'
 sitemap_url_scheme = '{link}'
 
 # Add any paths that contain templates here, relative to this directory.
