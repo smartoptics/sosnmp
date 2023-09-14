fc9c5fb6e6512ccd1520e43fd1c48b61c367df90
Lex Li <support@lextm.com>
2023-09-14 02:53:38 -0400

    Added initial unit test cases.

---
+++ tests/hlapi/asyncio/agent/ntforg/test_default-v1-trap.py
@@ -0,0 +1,21 @@
+import pytest
+from pysnmp.hlapi.asyncio import *
+
+@pytest.mark.asyncio
+async def test_send_notification():
+    snmpEngine = SnmpEngine()
+    errorIndication, errorStatus, errorIndex, varBinds = await sendNotification(
+        snmpEngine,
+        CommunityData('public', mpModel=0),
+        UdpTransportTarget(('demo.pysnmp.com', 162)),
+        ContextData(),
+        "trap",
+        NotificationType(ObjectIdentity("1.3.6.1.6.3.1.1.5.2")).addVarBinds(
+            ("1.3.6.1.6.3.1.1.4.3.0", "1.3.6.1.4.1.20408.4.1.1.2"),
+            ("1.3.6.1.2.1.1.1.0", OctetString("my system")),
+        ),
+    )
+    assert errorIndication is None
+    assert errorStatus == 0
+    assert errorIndex == 0
+    assert varBinds == []
---
+++ tests/hlapi/asyncio/agent/ntforg/test_v3-inform.py
@@ -0,0 +1,25 @@
+import pytest
+from pysnmp.hlapi import *
+from pysnmp.hlapi.asyncio.ntforg import sendNotification
+from pysnmp.hlapi.asyncio.transport import UdpTransportTarget
+from pysnmp.proto.errind import RequestTimedOut
+
+@pytest.mark.asyncio
+async def test_send_v3_inform_notification():
+    errorIndication, errorStatus, errorIndex, varBinds = await sendNotification(
+        SnmpEngine(),
+        UsmUserData('usr-md5-des', 'authkey1', 'privkey1'),
+        UdpTransportTarget(('demo.pysnmp.com', 162)),
+        ContextData(),
+        'inform',
+        NotificationType(
+            ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+        ).addVarBinds(
+            ('1.3.6.1.2.1.1.1.0', OctetString('my system'))
+        )
+    )
+
+    assert isinstance(errorIndication, RequestTimedOut)
+    assert errorStatus == 0
+    assert errorIndex == 0
+    assert varBinds == []
