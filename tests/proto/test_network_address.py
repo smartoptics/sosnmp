4a839c7219de4d896da022329b960418038e8159
Rares David <davidrares10@gmail.com>
2024-06-27 23:53:29 +0200

    added tests

---
+++ tests/proto/test_network_address.py
@@ -0,0 +1,44 @@
+import pytest
+from pyasn1.type.namedtype import NamedType
+
+from pysnmp.proto.rfc1155 import NetworkAddress, IpAddress, TypeCoercionHackMixIn
+
+
+def test_clone_none():
+    n = NetworkAddress()
+
+    assert n.clone().prettyPrint() == n.prettyPrint()
+
+
+def test_clone_NetworkAddress():
+    n = NetworkAddress()
+
+    assert n.clone(n.clone("10.10.10.10")).getName() == "internet"
+
+
+def test_clone_IpAddress():
+    ip = IpAddress("10.10.10.10")
+    n = NetworkAddress()
+
+    assert n.clone(ip).getName() == "internet"
+
+
+def test_clone_string():
+    n = NetworkAddress()
+
+    assert n.clone("10.10.10.10").getName() == "internet"
+
+
+def test_verifyComponent_normal():
+    t = TypeCoercionHackMixIn()
+    t._componentType = [NamedType("internet", IpAddress("10.10.10.10"))]
+
+    t._verifyComponent(0, IpAddress("10.2.3.4"))
+
+
+def test_verifyComponent_invalidIdx():
+    t = TypeCoercionHackMixIn()
+    t._componentType = [NamedType("internet", IpAddress("10.10.10.10"))]
+
+    with pytest.raises(Exception):
+        t._verifyComponent(1, IpAddress("10.2.3.4"))
