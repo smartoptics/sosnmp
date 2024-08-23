etingof/pysnmp

many backward-compatibility aids dropped

pysnmp/entity/config.py
@@ -5,7 +5,7 @@
 # License: http://snmplabs.com/pysnmp/license.html
 #
 from pyasn1.compat.octets import null
-from pysnmp.carrier.asyncore.dgram import udp, udp6, unix
+from pysnmp.carrier.asyncore.dgram import udp, udp6
 from pysnmp.proto.secmod.rfc3414.auth import hmacmd5, hmacsha, noauth
 from pysnmp.proto.secmod.rfc3414.priv import des, nopriv
 from pysnmp.proto.secmod.rfc3826.priv import aes
@@ -19,7 +19,6 @@ from pysnmp import error
 # Transports
 snmpUDPDomain = udp.snmpUDPDomain
 snmpUDP6Domain = udp6.snmpUDP6Domain
-snmpLocalDomain = unix.snmpLocalDomain
 
 # Auth protocol
 usmHMACMD5AuthProtocol = hmacmd5.HmacMd5.serviceID
@@ -129,15 +128,11 @@ def addV3User(snmpEngine, userName,
               authProtocol=usmNoAuthProtocol, authKey=None,
               privProtocol=usmNoPrivProtocol, privKey=None,
               securityEngineId=None,
-              securityName=None,
-              # deprecated parameters follow
-              contextEngineId=None):
+              securityName=None):
     mibBuilder = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder
 
     if securityName is None:
         securityName = userName
-    if securityEngineId is None:  # backward compatibility
-        securityEngineId = contextEngineId
     (snmpEngineID, usmUserEntry, tblIdx1,
      pysnmpUsmSecretEntry, tblIdx2) = __cookV3UserInfo(snmpEngine, userName, securityEngineId)
 
@@ -203,11 +198,7 @@ def addV3User(snmpEngine, userName,
 
 def delV3User(snmpEngine,
               userName,
-              securityEngineId=None,
-              # deprecated parameters follow
-              contextEngineId=None):
-    if securityEngineId is None:  # backward compatibility
-        securityEngineId = contextEngineId
+              securityEngineId=None):
     (snmpEngineID, usmUserEntry, tblIdx1, pysnmpUsmSecretEntry,
      tblIdx2) = __cookV3UserInfo(snmpEngine, userName, securityEngineId)
     snmpEngine.msgAndPduDsp.mibInstrumController.writeVars(
@@ -556,40 +547,6 @@ def delVacmUser(snmpEngine, securityModel, securityName, securityLevel,
         delVacmView(
             snmpEngine, notifyView, notifySubTree
         )
-
-
-# Obsolete shortcuts for add/delVacmUser() wrappers
-
-def addRoUser(snmpEngine, securityModel, securityName, securityLevel, subTree):
-    addVacmUser(snmpEngine, securityModel, securityName,
-                securityLevel, subTree)
-
-
-def delRoUser(snmpEngine, securityModel, securityName, securityLevel, subTree):
-    delVacmUser(snmpEngine, securityModel, securityName, securityLevel,
-                subTree)
-
-
-def addRwUser(snmpEngine, securityModel, securityName, securityLevel, subTree):
-    addVacmUser(snmpEngine, securityModel, securityName, securityLevel,
-                subTree, subTree)
-
-
-def delRwUser(snmpEngine, securityModel, securityName, securityLevel, subTree):
-    delVacmUser(snmpEngine, securityModel, securityName, securityLevel,
-                subTree, subTree)
-
-
-def addTrapUser(snmpEngine, securityModel, securityName,
-                securityLevel, subTree):
-    addVacmUser(snmpEngine, securityModel, securityName, securityLevel,
-                (), (), subTree)
-
-
-def delTrapUser(snmpEngine, securityModel, securityName,
-                securityLevel, subTree):
-    delVacmUser(snmpEngine, securityModel, securityName, securityLevel,
-                (), (), subTree)
 
 
 # Notification target setup

pysnmp/proto/secmod/rfc2576.py
@@ -8,7 +8,7 @@ import sys
 from pyasn1.codec.ber import encoder
 from pyasn1.error import PyAsn1Error
 from pysnmp.proto.secmod import base
-from pysnmp.carrier.asyncore.dgram import udp, udp6, unix
+from pysnmp.carrier.asyncore.dgram import udp, udp6
 from pysnmp.smi.error import NoSuchInstanceError
 from pysnmp.proto import errind, error
 from pysnmp import debug
@@ -142,8 +142,6 @@ class SnmpV1SecurityModel(base.AbstractSecurityModel):
                     TransportAddressIPv6, = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
                         'TRANSPORT-ADDRESS-MIB', 'TransportAddressIPv6')
                     targetAddrTAddress = tuple(TransportAddressIPv6(targetAddrTAddress))
-                elif targetAddrTDomain[:len(unix.snmpLocalDomain)] == unix.snmpLocalDomain:
-                    targetAddrTAddress = str(targetAddrTAddress)
                 targetAddr = targetAddrTDomain, targetAddrTAddress
                 targetAddrTagList = snmpTargetAddrTagList.getNode(snmpTargetAddrTagList.name + instId).syntax
                 if targetAddr not in self.__transportToTagMap:

smartoptics/sosnmp

pysnmp/entity/config.py
@@ -6,7 +6,7 @@
 #
 import warnings
 from pyasn1.compat.octets import null
-from pysnmp.carrier.asyncio.dgram import udp, udp6, unix
+from pysnmp.carrier.asyncio.dgram import udp, udp6
 from pysnmp.carrier.base import AbstractTransport
 from pysnmp.entity.engine import SnmpEngine
 from pysnmp.proto.secmod.rfc3414.auth import hmacmd5, hmacsha, noauth
@@ -63,7 +63,6 @@ def __getattr__(attr: str):
 # Transports
 snmpUDPDomain = udp.SNMP_UDP_DOMAIN
 snmpUDP6Domain = udp6.SNMP_UDP6_DOMAIN
-snmpLocalDomain = unix.SNMP_LOCAL_DOMAIN
 
 # Auth protocol
 USM_AUTH_HMAC96_MD5 = hmacmd5.HmacMd5.SERVICE_ID
@@ -238,16 +237,11 @@ def addV3User(
     securityName=None,
     authKeyType=USM_KEY_TYPE_PASSPHRASE,
     privKeyType=USM_KEY_TYPE_PASSPHRASE,
-    # deprecated parameter
-    contextEngineId=None,
 ):
     mibBuilder = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder
 
     if securityName is None:
         securityName = userName
-
-    if securityEngineId is None:  # backward compatibility
-        securityEngineId = contextEngineId
 
     (
         securityEngineId,
@@ -397,8 +391,6 @@ def delV3User(
     snmpEngine,
     userName,
     securityEngineId=None,
-    # deprecated parameters follow
-    contextEngineId=None,
 ):
     if securityEngineId is None:  # backward compatibility
         securityEngineId = contextEngineId
@@ -864,93 +856,6 @@ def delVacmUser(
         delVacmView(snmpEngine, writeView, writeSubTree)
     if notifySubTree:
         delVacmView(snmpEngine, notifyView, notifySubTree)
-
-
-# Obsolete shortcuts for add/delVacmUser() wrappers
-
-
-def addRoUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    addVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        subTree,
-        contextName=contextName,
-    )
-
-
-def delRoUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    delVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        subTree,
-        contextName=contextName,
-    )
-
-
-def addRwUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    addVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        subTree,
-        subTree,
-        contextName=contextName,
-    )
-
-
-def delRwUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    delVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        subTree,
-        subTree,
-        contextName=contextName,
-    )
-
-
-def addTrapUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    addVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        (),
-        (),
-        subTree,
-        contextName=contextName,
-    )
-
-
-def delTrapUser(
-    snmpEngine, securityModel, securityName, securityLevel, subTree, contextName=null
-):
-    delVacmUser(
-        snmpEngine,
-        securityModel,
-        securityName,
-        securityLevel,
-        (),
-        (),
-        subTree,
-        contextName=contextName,
-    )
 
 
 # Notification target setup

pysnmp/proto/secmod/rfc2576.py
@@ -8,7 +8,7 @@ import sys
 from pyasn1.codec.ber import encoder
 from pyasn1.error import PyAsn1Error
 from pysnmp.proto.secmod import base
-from pysnmp.carrier.asyncio.dgram import udp, udp6, unix
+from pysnmp.carrier.asyncio.dgram import udp, udp6
 from pysnmp.smi.error import NoSuchInstanceError
 from pysnmp.proto import errind, error
 from pysnmp import debug
@@ -207,12 +207,6 @@ class SnmpV1SecurityModel(base.AbstractSecurityModel):
                         "TRANSPORT-ADDRESS-MIB", "TransportAddressIPv6"
                     )
                     targetAddrTAddress = tuple(TransportAddressIPv6(targetAddrTAddress))
-                elif (
-                    targetAddrTDomain[: len(unix.SNMP_LOCAL_DOMAIN)]
-                    == unix.SNMP_LOCAL_DOMAIN
-                ):
-                    targetAddrTAddress = str(targetAddrTAddress)
-
                 targetAddr = targetAddrTDomain, targetAddrTAddress
                 targetAddrTagList = snmpTargetAddrTagList.getNode(
                     snmpTargetAddrTagList.name + instId