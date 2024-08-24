etingof/pysnmp

Fix `MibBuilder.loadModules()` no respect missing MIB failure
Missing MIB condition has been ignored if MIB compiler is not
configured.

pysnmp/smi/builder.py
@@ -419,23 +419,25 @@ class MibBuilder(object):
                 self.loadModule(modName, **userCtx)
 
             except error.MibNotFoundError:
-                if self._mibCompiler:
-                    debug.logger & debug.FLAG_BLD and debug.logger(
-                        'loadModules: calling MIB compiler for %s' % modName)
+                if not self._mibCompiler:
+                    raise
 
-                    status = self._mibCompiler.compile(modName, genTexts=self.loadTexts)
+                debug.logger & debug.FLAG_BLD and debug.logger(
+                    'loadModules: calling MIB compiler for %s' % modName)
 
-                    errs = '; '.join(
-                        hasattr(x, 'error') and str(x.error) or x
-                        for x in status.values()
-                        if x in ('failed', 'missing'))
+                status = self._mibCompiler.compile(modName, genTexts=self.loadTexts)
 
-                    if errs:
-                        raise error.MibNotFoundError(
-                            '%s compilation error(s): %s' % (modName, errs))
+                errs = '; '.join(
+                    hasattr(x, 'error') and str(x.error) or x
+                    for x in status.values()
+                    if x in ('failed', 'missing'))
 
-                    # compilation succeeded, MIB might load now
-                    self.loadModule(modName, **userCtx)
+                if errs:
+                    raise error.MibNotFoundError(
+                        '%s compilation error(s): %s' % (modName, errs))
+
+                # compilation succeeded, MIB might load now
+                self.loadModule(modName, **userCtx)
 
         return self
 

smartoptics/sosnmp

pysnmp/smi/builder.py
@@ -382,27 +382,32 @@ class MibBuilder:
                 self.loadModule(modName, **userCtx)
 
             except error.MibNotFoundError:
-                if self.__mibCompiler:
-                    debug.logger & debug.FLAG_BLD and debug.logger(
-                        "loadModules: calling MIB compiler for %s" % modName
-                    )
-                    status = self.__mibCompiler.compile(
-                        modName, genTexts=self.loadTexts
-                    )
-                    errs = "; ".join(
-                        [
-                            hasattr(x, "error") and str(x.error) or x
-                            for x in status.values()
-                            if x in ("failed", "missing")
-                        ]
-                    )
-                    if errs:
-                        raise error.MibNotFoundError(
-                            f"{modName} compilation error(s): {errs}"
-                        )
+                if not self.__mibCompiler:
+                    raise
 
-                    # compilation succeeded, MIB might load now
-                    self.loadModule(modName, **userCtx)
+                debug.logger & debug.FLAG_BLD and debug.logger(
+                    "loadModules: calling MIB compiler for %s" % modName
+                )
+                status = self.__mibCompiler.compile(modName, genTexts=self.loadTexts)
+                errs = "; ".join(
+                    [
+                        hasattr(x, "error") and str(x.error) or x
+                        for x in status.values()
+                        if x in ("failed", "missing")
+                    ]
+                )
+                if errs:
+                    raise error.MibNotFoundError(
+                        f"{modName} compilation error(s): {errs}"
+                    )
+
+                if errs:
+                    raise error.MibNotFoundError(
+                        f"{modName} compilation error(s): {errs}"
+                    )
+
+                # compilation succeeded, MIB might load now
+                self.loadModule(modName, **userCtx)
 
         return self
 