8797c58ad2529cf5980ab36ff8d41e1ad5b41ae7
Lex Li <support@lextm.com>
2024-09-14 02:50:30 -0400

    Moved test cases to v3arch.

--- tests/hlapi/v3arch/asyncio/manager/cmdgen/test_v2c_bulk.py
+++ tests/hlapi/v3arch/asyncio/manager/cmdgen/test_v2c_bulk.py
@@ -2,9 +2,6 @@ import pytest
 
 from pysnmp.hlapi.v3arch.asyncio import *
 from tests.agent_context import AGENT_PORT, AgentContextManager
-import pytest
-from pysnmp.hlapi.v3arch.asyncio import *
-from tests.agent_context import AGENT_PORT, AgentContextManager
 
 
 @pytest.mark.asyncio
@@ -24,12 +21,12 @@ async def test_v2c_bulk(num_bulk):
 
             assert errorIndication is None
             assert errorStatus == 0
-            assert len(varBinds) == 1
-            assert varBinds[0][0][0].prettyPrint() == "SNMPv2-MIB::sysObjectID.0"
+            assert len(varBinds) == num_bulk
+            assert varBinds[0][0].prettyPrint() == "SNMPv2-MIB::sysObjectID.0"
             if num_bulk > 1:
-                assert varBinds[0][1][0].prettyPrint() == "SNMPv2-MIB::sysUpTime.0"
+                assert varBinds[1][0].prettyPrint() == "SNMPv2-MIB::sysUpTime.0"
             if num_bulk > 2:
-                assert varBinds[0][2][0].prettyPrint() == "SNMPv2-MIB::sysContact.0"
+                assert varBinds[2][0].prettyPrint() == "SNMPv2-MIB::sysContact.0"
 
 
 @pytest.mark.asyncio
@@ -47,8 +44,7 @@ async def test_v2c_bulk_multiple_input():
 
             assert errorIndication is None
             assert errorStatus == 0
-            assert len(varBinds) == 1
-            assert len(varBinds[0]) == 5
+            assert len(varBinds) == 5
 
 
 # snmpbulkget -v2c -c public -C n1 -C r2 localhost 1.3.6.1.2.1.1.4 1.3.6.1.2.1.1.9.1.1 1.3.6.1.2.1.1.9.1.3
