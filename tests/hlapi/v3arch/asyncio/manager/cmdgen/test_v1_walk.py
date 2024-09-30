58b4a873478ad8c694f25d6c1261f92b3efd16bc
Lex Li <support@lextm.com>
2024-09-30 13:44:05 -0400

    Added more MIB related test cases.

--- tests/hlapi/v3arch/asyncio/manager/cmdgen/test_v1_walk.py
+++ tests/hlapi/v3arch/asyncio/manager/cmdgen/test_v1_walk.py
@@ -1,5 +1,7 @@
 import pytest
 from pysnmp.hlapi.v3arch.asyncio import *
+from pysnmp.smi.builder import MibBuilder
+from pysnmp.smi.view import MibViewController
 from tests.agent_context import AGENT_PORT, AgentContextManager
 
 
@@ -32,6 +34,64 @@ async def test_v1_walk():
         assert varBinds[0][0].prettyPrint() == "SNMPv2-MIB::sysUpTime.0"
 
         assert len(objects_list) == 267
+
+        snmpEngine.closeDispatcher()
+
+
+@pytest.mark.asyncio
+async def test_v1_walk_mib():
+    async with AgentContextManager():
+        mib_builder = MibBuilder()
+        mib_view_controller = MibViewController(mib_builder)
+        mib_builder.loadModules(
+            "SNMP-COMMUNITY-MIB",
+            "PYSNMP-MIB",
+            "PYSNMP-USM-MIB",
+            "SNMP-VIEW-BASED-ACM-MIB",
+        )
+
+        snmpEngine = SnmpEngine()
+        snmpEngine.cache["mibViewController"] = mib_view_controller
+        objects = walkCmd(
+            snmpEngine,
+            CommunityData("public", mpModel=0),
+            await UdpTransportTarget.create(("localhost", AGENT_PORT)),
+            ContextData(),
+            ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+        )
+
+        objects_list = [item async for item in objects]
+
+        errorIndication, errorStatus, errorIndex, varBinds = objects_list[0]
+
+        assert errorIndication is None
+        assert errorStatus == 0
+        assert len(varBinds) == 1
+        assert varBinds[0][0].prettyPrint() == "SNMPv2-MIB::sysObjectID.0"
+
+        errorIndication, errorStatus, errorIndex, varBinds = objects_list[1]
+
+        assert errorIndication is None
+        assert errorStatus == 0
+        assert len(varBinds) == 1
+        assert varBinds[0][0].prettyPrint() == "SNMPv2-MIB::sysUpTime.0"
+
+        assert len(objects_list) == 267
+
+        errorIndication, errorStatus, errorIndex, varBinds = objects_list[-1]
+        assert (
+            varBinds[0][0].prettyPrint()
+            == 'SNMP-COMMUNITY-MIB::snmpCommunityStatus."public"'
+        )
+
+        for errorIndication, errorStatus, errorIndex, varBinds in objects_list:
+            content = varBinds[0][0].prettyPrint()
+            if (
+                not content.startswith("PYSNMP-USM-MIB::")
+                and not content.startswith("SNMP-USER-BASED-SM-MIB::")
+                and not content.startswith("SNMP-VIEW-BASED-ACM-MIB::")
+            ):
+                assert content.count(".") == 1  # fully resolved.
 
         snmpEngine.closeDispatcher()
 
